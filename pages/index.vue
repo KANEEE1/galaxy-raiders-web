558210daf2e445fcabe966a95498b2389c72def0
KANEEE1 <felipek.souza@usp.br>
2023-07-13 16:18:56 -0300

    EP finalizado

--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,75 @@
 <template>
-  <div id="canvas">
-    <div id="deep-space" />
-    <div id="space-field">
-      <SpaceObject id="spaceship" class="spaceship" :data="spaceField.ship" resolution="2" />
-
-      <SpaceObject class="asteroid" :data="asteroid" resolution="2"
-                  :key="asteroid.center"
-                  v-for="asteroid in spaceField.asteroids" />
-
-      <SpaceObject class="missile" :data="missile" resolution="2"
-                  :key="missile.center"
-                  v-for="missile in spaceField.missiles" />
-
-      <SpaceObject class="explosion" :data="explosion" resolution="2"
-                  :key="explosion.center"
-                  v-for="explosion in spaceField.explosions" />
-    </div>
+  <div class="menu-container">
+    <h1 class="title">Menu</h1>
+    <ul class="menu-list">
+      <li>
+        <nuxt-link to="/game" class="menu-link">Iniciar partida</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/leaderboard" class="menu-link">Placar</nuxt-link>
+      </li>
+      <li>
+        <a href="#" @click.prevent="exitGame" class="menu-link">Sair</a>
+      </li>
+    </ul>
   </div>
 </template>
 
-<script setup>
-const {
-  data: spaceField,
-  refresh: updateSpaceField
-} = await $get("/space-field");
-
-onMounted(() => {
-  window.addEventListener("keydown", async (event) => {
-    const keyToCommand = {
-      "ArrowUp": "MOVE_SHIP_UP",
-      "ArrowDown": "MOVE_SHIP_DOWN",
-      "ArrowRight": "MOVE_SHIP_RIGHT",
-      "ArrowLeft": "MOVE_SHIP_LEFT",
-      "Space": "LAUNCH_MISSILE",
-      "Escape": "PAUSE_GAME",
-    };
-
-    const command = keyToCommand[event.code];
-
-    // Ignore if invalid key was pressed
-    if (command === undefined) return;
-
-    console.log(`Triggering command: ${command}`);
-    await $post("/ship/commands", { command })
-  });
-
-  window.setInterval(updateSpaceField, 1000);
-})
+<script>
+export default {
+  methods: {
+    exitGame() {
+      window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }
+  }
+};
 </script>
 
 <style>
-#canvas {
-  height: calc(100vh - 4rem);
-  width: calc(100vw - 4rem);
-
-  padding: 2rem;
-
-  background-color: #36bbf5;
-  overflow: hidden;
-
-  position: relative;
-
+.menu-container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: url('~/assets/space.gif');
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.title {
+  font-size: 48px;
+  color: #fff;
+  margin-bottom: 40px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+.menu-list {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
   align-items: center;
 }
 
-@keyframes slide {
-  0% {
-    transform: translate(1px);
-  }
-  50% {
-    transform: translate(-1px);
-  }
-  100% {
-    transform: translate(1px);
-  }
+.menu-link {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  padding: 20px;
+  width: 200px; /* Largura dos botões */
+  font-size: 24px;
+  color: #fff;
+  background-color: #007bff;
+  text-decoration: none;
+  border-radius: 5px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  transition: background-color 0.3s ease;
 }
 
-#deep-space {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
-  background-image: url("~/assets/space.png");
-  background-origin: content-box;
-  animation: slide 3s linear infinite;
-
-  position: absolute;
-  z-index: 0;
-}
-
-#space-field {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
-  position: relative;
-}
-
-.spaceship {
-  background-image: url("~/assets/spaceship.png");
-}
-
-.asteroid {
-  background-image: url("~/assets/asteroid.png");
-}
-
-.missile {
-  background-image: url("~/assets/missile.png");
-}
-
-.explosion {
-  background-image: url("~/assets/explosion.png");
+.menu-link:hover {
+  background-color: #0056b3;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
 }
 </style>
---
+++ pages/leaderboard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="table-container">
+    <table>
+      <thead>
+        <tr>
+          <th>Colocação</th>
+          <th>Data</th>
+          <th>Meteoros Destruídos</th>
+          <th>Pontuação</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(game, index) in games" :key="index" :class="index % 2 === 0 ? 'even-row' : 'odd-row'">
+<td>{{ index + 1 }}</td>
+<td>{{ new Date(game.timestamp).toLocaleDateString() }}</td>
+<td>{{ game.asteroidsDestroyed }}</td>
+<td>{{ game.finalScore }}</td>
+</tr>
+</tbody>
+</table>
+  </div>
+</template>
+<script>
+import data from '../mock-leaderboard.json';
+
+export default {
+  data() {
+    return {
+      games: [],
+    };
+  },
+  mounted() {
+    this.games = data.games;
+  },
+};
+</script>
+<style scoped>
+.table-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: url('~/assets/space.gif') no-repeat center center fixed;
+  background-size: cover;
+  font-size: 2.5em;
+}
+
+table {
+  width: 80%;  
+  height: 80%; 
+  border-collapse: separate;
+  border-spacing: 0;
+  border: 5px solid white; /* sublinhado branco */
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: #0000FF; /* Cor de fundo azul */
+}
+
+th, td {
+  border: 1px solid #000000; /* Borda branca */
+  padding: 12px; 
+  text-align: center;
+  color: #000000; /* Cor do texto branco */
+}
+
+th {
+  background-color: #0000FF; /* Cabeçalho azul */
+  color: white;
+}
+
+tr {
+  background-color: #FFFFFF; /* Cor de fundo das linhas pares verde */
+}
+</style>
